3c86f063bf6d25ecd8ebbb0133268e3c101324d2
Barath P <p.barath1374@gmail.com>
2026-04-17 17:10:57 +0530

    v1 - final production

--- src/main/java/com/atmymelo/atmymelobackend/config/WebConfig.java
+++ src/main/java/com/atmymelo/atmymelobackend/config/WebConfig.java
@@ -13,9 +13,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://atmymelo-frontend.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
